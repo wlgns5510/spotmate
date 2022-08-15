5d6ec211574c43a01fe6d743143aa3d468f48708
ujiun <ujiun@hanmail.net>
2022-08-16 04:58:13 +0900

    페이징

--- src/main/java/com/spotmate/service/MypageJService.java
+++ src/main/java/com/spotmate/service/MypageJService.java
@@ -17,18 +17,83 @@ public class MypageJService {
 	private MypageJDao mypagejDao;
 
 	// 쿠폰리스트가져오기
-	public List<CouponVo> getCouponList(String startDate, String endDate, String option1, String option2) {
+	public Map<String, Object> getCouponList(String startDate, String endDate, String option1, String option2, int crtPage) {
 		System.out.println("MypageJService > getCouponList");
+		
+		/////////////리스트//////////////
+		
+		//페이지당 글갯수
+		int listCnt = 10;
+		
+		//전체페이지
+		crtPage = (crtPage > 0) ? crtPage : (crtPage = 1);
+		
+		//시작글번호
+		int startRnum = (crtPage - 1)*listCnt + 1;
+		
+		//끝글번호
+		int endRnum = (startRnum + listCnt) - 1;
+		
+		
+		Map<String, Object> pMap = new HashMap<String, Object>();
+		pMap.put("startDate", startDate);
+		pMap.put("endDate", endDate);
+		pMap.put("option1", option1);
+		pMap.put("option2", option2);
+		pMap.put("startRnum", startRnum);
+		pMap.put("endRnum", endRnum);
 
-		Map<String, Object> pmap = new HashMap<String, Object>();
-		pmap.put("startDate", startDate);
-		pmap.put("endDate", endDate);
-		pmap.put("option1", option1);
-		pmap.put("option2", option2);
+		List<CouponVo> couponList = mypagejDao.getCouponList(pMap);
 
-		List<CouponVo> couponList = mypagejDao.getCouponList(pmap);
-
-		return couponList;
+		/////////////페이징계산//////////////
+		
+		//전체글갯수
+		int totalCouponCnt = mypagejDao.totalCouponCnt();
+		System.out.println(totalCouponCnt);
+		
+		//페이지당 버튼 갯수
+		int pageBtnCount = 10;
+		
+		//마지막 버튼 번호
+		int endPageBtnNo = (int)Math.ceil(crtPage/(double)pageBtnCount)*pageBtnCount;
+		
+		//시작 버튼 번호
+		int startPageBtnNo = (endPageBtnNo - pageBtnCount) + 1;
+		
+		System.out.println(crtPage + "," + startPageBtnNo + "," + endPageBtnNo);
+		
+		//다음 화살표 유무
+		boolean next = false;
+		if(listCnt * endPageBtnNo < totalCouponCnt) {
+			next = true;
+		
+		}else {
+			endPageBtnNo = (int)Math.ceil(totalCouponCnt/(double)listCnt);
+		
+		}
+		
+		//이전 화살표 유무
+		boolean prev = false;
+		if(startPageBtnNo !=1) {
+			prev = true;
+			
+		}
+		
+		//마지막페이지번호
+		int endPageNo = (int)Math.ceil(totalCouponCnt/(double)listCnt);
+		
+		System.out.println(prev + "," + startPageBtnNo + "," + endPageBtnNo + "," + next + "," + endPageNo);
+		
+		Map<String, Object> cMap = new HashMap<String, Object>();
+		cMap.put("couponList", couponList);
+		cMap.put("prev", prev);
+		cMap.put("next", next);
+		cMap.put("endPageBtnNo", endPageBtnNo);
+		cMap.put("startPageBtnNo", startPageBtnNo);
+		cMap.put("endPageNo", endPageNo);
+		
+		
+		return cMap;
 
 	}
 
@@ -38,5 +103,9 @@ public class MypageJService {
 
 		mypagejDao.chargePoint(point);
 	}
+	
+
+		
+	
 
 }
