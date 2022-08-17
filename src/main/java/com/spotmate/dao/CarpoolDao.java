0e56671e723fb8999774ccd3e60fd9f0ed9accfe
LJH0716 <jhmk0805@naver.com>
2022-08-17 17:37:35 +0900

    테스트

--- src/main/java/com/spotmate/dao/CarpoolDao.java
+++ src/main/java/com/spotmate/dao/CarpoolDao.java
@@ -11,18 +11,55 @@ import com.spotmate.vo.CarpoolVo;
 @Repository
 public class CarpoolDao {
 	
-	@Autowired
-	private SqlSession ss;
-	
-	public List<CarpoolVo> selectList() {
-		List<CarpoolVo> csL = ss.selectList("carpool.selectslist");
-		List<CarpoolVo> ceL = ss.selectList("carpool.selectelist");
-		for (int i=0;i<csL.size();i++) {
-			if (csL.get(i).getNo() == ceL.get(i).getNo()) {
-				csL.get(i).setSplace(csL.get(i).getPlace());
-				csL.get(i).setEplace(ceL.get(i).getPlace());
+		
+		@Autowired
+		private SqlSession sqlSession;
+		
+		/*
+		// 리스트
+		public List<CarpoolVo> getcarpoolList() {
+			System.out.println("CarpoolDao>list()");
+
+			List<CarpoolVo> carpoolList = sqlSession.selectList("carpool.selectCarpoolList");
+			System.out.println(carpoolList);
+
+			return carpoolList;
+		}*/
+		
+		/*public List<CarpoolVo> getcarpoolList(String keyword) {			
+			System.out.println("CarpoolDao>list()");
+			
+			List<CarpoolVo> csL = sqlSession.selectList("carpool.selectslist", keyword);
+			List<CarpoolVo> ceL = sqlSession.selectList("carpool.selectelist", keyword);
+			for (int i=0; i<csL.size(); i++) {
+				if (csL.get(i).getNo() == ceL.get(i).getNo()) {
+					csL.get(i).setSplace(csL.get(i).getPlace());
+					csL.get(i).setEplace(ceL.get(i).getPlace());
+				}
 			}
+			return csL;
 		}
-		return csL;
-	}
+		*/
+		
+		//리뷰 리스트
+		public List<CarpoolVo> getreviewList() {
+			System.out.println("CarpoolDao>list()");
+
+			List<CarpoolVo> reviewList = sqlSession.selectList("carpool.selectReviewlist");
+			System.out.println(reviewList);
+
+			return reviewList;
+		}
+		
+		
+		//드라이버 정보 가져오기
+		public CarpoolVo read(int no) {
+			System.out.println("carpoolDao>read()");
+
+			return sqlSession.selectOne("carpool.selectDriverInfo", no);
+		}
+		
+	
+
 }
+
--- src/main/java/com/spotmate/service/CarpoolService.java
+++ src/main/java/com/spotmate/service/CarpoolService.java
@@ -11,11 +11,121 @@ import com.spotmate.vo.CarpoolVo;
 @Service
 public class CarpoolService {
 	
-	@Autowired
-	private CarpoolDao cD;
-	
-	public List<CarpoolVo> getCarpoolList() {
-		return cD.selectList();
-	}
 
+	@Autowired
+	private CarpoolDao carpoolDao;
+	
+	/*
+	// 리스트
+	public List<CarpoolVo> getList() {
+		System.out.println("CarpoolService > getList");
+
+		List<CarpoolVo> carpoolList = carpoolDao.getcarpoolList();
+		System.out.println(carpoolList);
+		
+		return carpoolList;
+	}*/
+	
+	/*
+	// 리스트가져오기
+		public Map<String, Object> getCouponList(String startDate, String endDate, String option1, String option2, int crtPage) {
+			System.out.println("MypageJService > getCouponList");
+			
+			/////////////리스트//////////////
+			
+			//페이지당 글갯수
+			int listCnt = 10;
+			
+			//전체페이지
+			crtPage = (crtPage > 0) ? crtPage : (crtPage = 1);
+			
+			//시작글번호
+			int startRnum = (crtPage - 1)*listCnt + 1;
+			
+			//끝글번호
+			int endRnum = (startRnum + listCnt) - 1;
+			
+			
+			Map<String, Object> pMap = new HashMap<String, Object>();
+			pMap.put("startDate", startDate);
+			pMap.put("endDate", endDate);
+			pMap.put("option1", option1);
+			pMap.put("option2", option2);
+			pMap.put("startRnum", startRnum);
+			pMap.put("endRnum", endRnum);
+
+			List<CouponVo> couponList = mypagejDao.getCouponList(pMap);
+
+			/////////////페이징계산//////////////
+			
+			//전체글갯수
+			int totalCouponCnt = mypagejDao.totalCouponCnt();
+			System.out.println(totalCouponCnt);
+			
+			//페이지당 버튼 갯수
+			int pageBtnCount = 10;
+			
+			//마지막 버튼 번호
+			int endPageBtnNo = (int)Math.ceil(crtPage/(double)pageBtnCount)*pageBtnCount;
+			
+			//시작 버튼 번호
+			int startPageBtnNo = (endPageBtnNo - pageBtnCount) + 1;
+			
+			System.out.println(crtPage + "," + startPageBtnNo + "," + endPageBtnNo);
+			
+			//다음 화살표 유무
+			boolean next = false;
+			if(listCnt * endPageBtnNo < totalCouponCnt) {
+				next = true;
+			
+			}else {
+				endPageBtnNo = (int)Math.ceil(totalCouponCnt/(double)listCnt);
+			
+			}
+			
+			//이전 화살표 유무
+			boolean prev = false;
+			if(startPageBtnNo !=1) {
+				prev = true;
+				
+			}
+			
+			//마지막페이지번호
+			int endPageNo = (int)Math.ceil(totalCouponCnt/(double)listCnt);
+			
+			
+			System.out.println(prev + "," + startPageBtnNo + "," + endPageBtnNo + "," + next + "," + endPageNo);
+			
+			Map<String, Object> cMap = new HashMap<String, Object>();
+			cMap.put("couponList", couponList);
+			cMap.put("prev", prev);
+			cMap.put("next", next);
+			cMap.put("endPageBtnNo", endPageBtnNo);
+			cMap.put("startPageBtnNo", startPageBtnNo);
+			cMap.put("endPageNo", endPageNo);
+			
+			
+			return cMap;
+
+		}
+		*/
+		
+	//리뷰 리스트
+	public List<CarpoolVo> getList2() {
+		System.out.println("ReviewService > getList");
+
+		List<CarpoolVo> reviewList = carpoolDao.getreviewList();
+		System.out.println(reviewList);
+		
+		return reviewList;
+	}
+	
+	//드라이버 차량 정보 가져오기
+	public CarpoolVo read(int no) {
+
+		System.out.println("CarpoolService>read()");
+		
+		return carpoolDao.read(no);
+	}
+	
 }
