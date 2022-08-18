0873811f9cb126d5c0fdb8613c11e19785674296
joyunju <whdbswn1113@naver.com>
2022-08-18 20:07:15 +0900

    UserController

--- src/main/java/com/spotmate/controller/UserController.java
+++ src/main/java/com/spotmate/controller/UserController.java
@@ -4,24 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spotmate.service.UserService;
-import com.spotmate.vo.UserVo;
 
 @Controller
 public class UserController {
-	
-	@Autowired
-	private UserService us;
-	
-	
-	@RequestMapping("/test")
-	public String users(Model model) {
-//		System.out.println("hi");
-		UserVo uVo = us.getInfo();
-		System.out.println(uVo);
-		model.addAttribute("user", us.getInfo());
-		return "/index";
-	}
+   
+   @Autowired
+   private UserService uService;
+   //localhost/loginForm
+   @RequestMapping(value = "/loginForm", method = { RequestMethod.GET, RequestMethod.POST })
+   public String users(Model model) {
+      return "/users/loginForm";
+   }
 
 }
