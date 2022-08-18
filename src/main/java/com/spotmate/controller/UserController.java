c6a54ca89982d07da9bb62fbde42fd6880a129fb
joyunju <whdbswn1113@naver.com>
2022-08-18 21:47:27 +0900

    UserController

--- src/main/java/com/spotmate/controller/UserController.java
+++ src/main/java/com/spotmate/controller/UserController.java
@@ -3,10 +3,12 @@ package com.spotmate.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spotmate.service.UserService;
+import com.spotmate.vo.UserVo;
 
 @Controller
 public class UserController {
@@ -23,6 +25,15 @@ public class UserController {
    @RequestMapping(value = "/joinForm", method = { RequestMethod.GET, RequestMethod.POST })
    public String join(Model model) {
       return "/users/joinForm";
+   }
+   
+   @RequestMapping(value = "/joinOk", method = { RequestMethod.GET, RequestMethod.POST })
+   public String joinOk(Model model, @ModelAttribute UserVo userVo) {
+	  
+	  System.out.println(userVo.toString());
+	  
+	  uService.joinUser(userVo);
+      return "/users/joinOk";
    }
 
 }
